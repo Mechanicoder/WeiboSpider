6639f77eaf343a68c60f26e7fd0992b431f81d84
Mechanicoder_h <mechanicoder@126.com>
2022-04-23 11:39:23 +0800

    保存html网页时使用原始编码

--- weibo_spider/parser/util.py
+++ weibo_spider/parser/util.py
@@ -19,21 +19,21 @@ def hash_url(url):
 
 def handle_html(cookie, url, type='selector'):
     """处理html"""
-    """处理html"""
     try:
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
         headers = {'User_Agent': user_agent, 'Cookie': cookie}
         resp = requests.get(url, headers=headers)
 
-        if GENERATE_TEST_DATA:
+        if GENERATE_TEST_DATA and resp.ok:
             import io
             import os
 
             resp_file = os.path.join(TEST_DATA_DIR, '%s.html' % hash_url(url))
-            with io.open(resp_file, 'w', encoding='utf-8') as f:
+            with io.open(resp_file, 'w', encoding=resp.encoding) as f:
                 f.write(resp.text)
 
             with io.open(os.path.join(TEST_DATA_DIR, URL_MAP_FILE), 'r+') as f:
+                # TODO：当文件内容为空时
                 url_map = json.loads(f.read())
                 url_map[url] = resp_file
                 f.seek(0)
